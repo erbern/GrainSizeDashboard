94bc69176a840ef3f847ddd44dc93d06e2a515a8
erbern <104540862+erbern@users.noreply.github.com>
2026-02-13 12:56:14 -0500

    Add files via upload

--- dashv1.py
+++ dashv1.py
@@ -230,4 +230,4 @@ def update_dashboard(boreholes, formations):
 # -----------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
